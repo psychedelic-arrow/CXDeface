463724aa1ff200ab1543f041042e78b84849276f
LOLGVH <77339814+LOLGVH@users.noreply.github.com>
2021-01-18 08:35:32 +0530

    Update Mass.sh

--- Mass.sh
+++ Mass.sh
@@ -191,7 +191,7 @@ fi
 
 if [ $apaan = "T" ] || [ $apaan = "t" ]
 then
-cat vul.txt
+cat cx remote.txt
 echo $cyan"["$yellow"B"$cyan"]"$yellow">>>>back"
 read -p "[ b ] " back             
 sh Mass.sh
